9244b5a796d6b85edff5c0f10c64d8f33ca246c7
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-21 22:44:42 +0530

    bouning balls

---
+++ src/Components/Buttons.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./buttons.css";
+function Buttons() {
+  return (
+    <div className="container">
+      <h1 className="button">Click me</h1>
+    </div>
+  );
+}
+
+export default Buttons;
