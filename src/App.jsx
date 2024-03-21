868319db523144a610f2f7f63f42fec933d250b5
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-22 05:02:46 +0530

    intial styles removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Buttons from "./Components/Buttons";
 import BouncingBalls from "./Components/BouncingBalls";
-
+import Hover from "./Components/Hover.jsx";
+// import Hover from "./Components/Hover";
 function App() {
   return (
     <div>
-      <BouncingBalls />
+      {/* <BouncingBalls /> */}
+      <Hover />
     </div>
   );
 }
---
+++ src/Components/Hover.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Hover() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Hover;
